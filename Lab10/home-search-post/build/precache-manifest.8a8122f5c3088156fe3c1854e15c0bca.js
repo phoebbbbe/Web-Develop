8f8aeaca14a1c64a2aaa53bd9af54eaee3c62a88
phoe_bbbbe <98581686+phoebbbbe@users.noreply.github.com>
2022-12-28 13:48:56 +0800

    upload Lab10

---
+++ Lab10/home-search-post/build/precache-manifest.8a8122f5c3088156fe3c1854e15c0bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac85eaeab4ada4b059a",
+    "url": "/static/css/main.2f0b11a6.chunk.css"
+  },
+  {
+    "revision": "5ac85eaeab4ada4b059a",
+    "url": "/static/js/main.5ac85eae.chunk.js"
+  },
+  {
+    "revision": "ea9aadbca85543b23937",
+    "url": "/static/js/1.ea9aadbc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b24395c2a62f4591f3984259840f362",
+    "url": "/index.html"
+  }
+];
