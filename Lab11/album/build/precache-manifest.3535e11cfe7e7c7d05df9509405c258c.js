3b4affa946f472fbc8a14386a157a542e8fbdd7a
phoe_bbbbe <98581686+phoebbbbe@users.noreply.github.com>
2022-12-30 15:27:09 +0800

    upload build folder

---
+++ Lab11/album/build/precache-manifest.3535e11cfe7e7c7d05df9509405c258c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0016c0fe443bbfd9d0f",
+    "url": "/static/css/main.6eda5b68.chunk.css"
+  },
+  {
+    "revision": "a0016c0fe443bbfd9d0f",
+    "url": "/static/js/main.a0016c0f.chunk.js"
+  },
+  {
+    "revision": "a54462eefa65024a4537",
+    "url": "/static/js/1.a54462ee.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39f03fc8865b8293d0e83fd63b918bf6",
+    "url": "/index.html"
+  }
+];
